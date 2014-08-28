27e7c20e328968d40e0f612b996802e6de858aab
drorzz <arhorus@gmail.com>
2014-08-29 01:35:07 +0300

    Update with reflection

--- src/main/java/org/drorzz/utils/OuterClassInfo.java
+++ src/main/java/org/drorzz/utils/OuterClassInfo.java
@@ -1,17 +1,50 @@
 package org.drorzz.utils;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author Drorzz
  * @since 28.08.2014.
  */
 public class OuterClassInfo {
+	private static class EmptyStackTraceThrowable extends Throwable{
+		public EmptyStackTraceThrowable() {
+		}
+	}
+
+	private final static int MAX_DEPTH = 3;
+	private final static Method nativeStackTraceElementMethod = initNativeStackTraceElementMethod();
+
+	private static Method initNativeStackTraceElementMethod(){
+		Method method;
+		try {
+
+			method = EmptyStackTraceThrowable.class.getSuperclass().getDeclaredMethod("getStackTraceElement", int.class);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+		method.setAccessible(true);
+		return method;
+	}
+
+	private static StackTraceElement getStackTraceElement(Throwable exception,int index) {
+		try {
+			return (StackTraceElement)nativeStackTraceElementMethod.invoke(exception,index);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
     private static StackTraceElement getClassInformer(){
-        StackTraceElement[] stack = new Throwable().getStackTrace();
+	    EmptyStackTraceThrowable emptyThrowable = new EmptyStackTraceThrowable();
         String className = OuterClassInfo.class.getName();
-        for(StackTraceElement element:stack){
+	    StackTraceElement element;
+	    for(int i = 2; i <= MAX_DEPTH; i++){
+		    element = getStackTraceElement(emptyThrowable,i);
             if(!className.equals(element.getClassName())){
-                return element;
+//	            System.out.println(i);
+	            return element;
             }
         }
         throw new RuntimeException("Not found outer class.");
@@ -39,3 +72,4 @@ public class OuterClassInfo {
         return getClassInformer().getMethodName();
     }
 }
+
